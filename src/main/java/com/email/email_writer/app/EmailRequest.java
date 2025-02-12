2a17eefcac1c3f90594e0d806e72fcfbcfeef160
vivek <vivek.jindal.sbg@gmail.com>
2025-02-12 21:52:41 +0530

    Initial commit

---
+++ src/main/java/com/email/email_writer/app/EmailRequest.java
@@ -0,0 +1,9 @@
+package com.email.email_writer.app;
+import lombok.Data;
+
+@Data
+public class EmailRequest
+{
+    private String EmailContent;
+    private String tone;
+}
---
+++ src/main/java/com/email/email_writer/app/Service.java
@@ -0,0 +1,4 @@
+package com.email.email_writer.app;
+
+public class Service {
+}
