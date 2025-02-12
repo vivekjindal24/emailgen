0a6b28605750f20e3ad416991f885092f2b3c99e
vivek <vivek.jindal.sbg@gmail.com>
2025-02-12 22:43:45 +0530

    Email Generator Prompt Built.

--- src/main/java/com/email/email_writer/app/EmailService.java
+++ src/main/java/com/email/email_writer/app/EmailService.java
@@ -5,4 +5,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService
 {
+    public String generateEmailReply(EmailRequest emailRequest)
+    {
+        String prompt = buildPrompt(EmailRequest);
+    }
+    private String buildPrompt(EmailRequest emailRequest)
+    {
+        StringBuilder propmt = new StringBuilder();
+        propmt.append("Generate a professional email replay for the following email. Please don't generate a subject line.");
+        propmt.append("\n");
+        if (emailRequest.getTone() != null && !emailRequest.getTone().isEmpty())
+        {
+            propmt.append("Use a").append(emailRequest.getTone()).append(" tone.");
+
+        }
+        propmt.append("\n Original email: \n").append(emailRequest.getEmailContent());
+
+        return propmt.toString();
+    }
 }
